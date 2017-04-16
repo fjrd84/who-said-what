880e1ad8a130a0e7bdfb2550990b2752f3123391
F. Javier R. Donado <fjrd84@gmail.com>
2017-04-16 13:36:31 +0200

    Test language data loader.

--- tests/text_analyzer_test.py
+++ tests/text_analyzer_test.py
@@ -5,12 +5,16 @@ Text analyzer tests.
 """
 from engines import text_analyzer
 
+START_WORDS_PATH = './tests/demo_start_words.txt'
+STOP_WORDS_PATH = './tests/demo_stop_words.txt'
+GRAMMAR_PATH = './tests/demo_grammar.txt'
+
 
 def test_file_parser():
     """
     File parser tests.
     """
-    start_words = text_analyzer.file_parser('./tests/demo_start_words.txt', True)
+    start_words = text_analyzer.file_parser(START_WORDS_PATH, True)
     assert 'negativity' in start_words
     assert 'pci' in start_words
     assert r'acute \w+' in start_words
@@ -19,3 +23,17 @@ def test_file_parser():
     assert 'yeast infections' in start_words
     assert 'wrinkless' not in start_words
     assert 'invented_word' not in start_words
+
+
+def test_language_data_loader():
+    """
+    Language data loader tests
+    """
+    language_data = text_analyzer.language_data_loader(
+        GRAMMAR_PATH,
+        START_WORDS_PATH,
+        STOP_WORDS_PATH)
+    assert 'eczema' in language_data['start_words']
+    assert '^@\\w+$' in language_data['stop_words']
+    assert '[s] \\w+ed to (healthier|better)( \\S+){0,7} [p]' in language_data['grammar']
+    assert r'[s] effective( \w+){0,2} (in|for|to)( \w+){0,5} [p]' in language_data['grammar']
