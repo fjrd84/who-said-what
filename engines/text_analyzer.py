9e8a4017cb2c07bd97a8bd1a3be97ba957028ebd
F. Javier R. Donado <fjrd84@gmail.com>
2017-04-16 13:06:35 +0200

    First refactor: file parser and nlp dependency injection.

--- engines/text_analyzer.py
+++ engines/text_analyzer.py
@@ -1,73 +1,68 @@
-# SpaCy is ON #
-
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-# Libraries:
-# from __future__ import unicode_literals
-import re
-#import sys
-from spacy.en import English
-# reload(sys)
-# sys.setdefaultencoding('utf8')
-path_to_language_data = './language_data/'
-# Function for text processing with Spacy:
-print('Loading libraries...')
-nlp = English()
-
+"""
 
 ##############
 ## Analyzer ##
 ##############
 
-# This program receives a text input (message), and extracts valuable
-# information from it within the DISEASE-TREATMENT cognitive frame, i.e. a
-# disease and a potential solution for it.
+Given a grammar, and a set of start words and end words, this
+file provides with the tools to analyze a human text written in
+English.
 
-# The Analyzer consists of 3 functions: (1) language_data_loader loads
-# linguistic knowledge to feed the Analyzer (a simple NLP engine), (2)
-# start_word_match finds disease mentions in the incoming text message,
-# (3) Analyzer uses information from previous inputs to extract entities
-# (syntactically, noun phrases) that semantically express solutions for a
-# mentioned disease.
+The analyzer receives a text input (message), and extracts valuable
+information from it within the DISEASE-TREATMENT cognitive frame, i.e. a
+disease and a potential solution for it.
 
-# (1) Language data loader:
+It consists of 3 functions: (1) language_data_loader loads
+linguistic knowledge to feed the Analyzer (a simple NLP engine), (2)
+start_word_match finds disease mentions in the incoming text message,
+(3) Analyzer uses information from previous inputs to extract entities
+(syntactically, noun phrases) that semantically express solutions for a
+mentioned disease.
 
-def language_data_loader():
-    language_data = dict()
-    # Load grammar:
-    grammar_file = open(path_to_language_data + 'grammar.txt', 'r')
+"""
+import re
+
+
+def file_parser(path, to_lower):
+    """
+    Helper function to parse text files.
+    """
+    grammar_file = open(path, 'r')
     grammar = []
     for l in grammar_file:
         l = l.strip()
+        if to_lower:
+            l = l.lower()
         grammar.append(l)
     grammar_file.close()
-    language_data['grammar'] = grammar
-    # Load start words (a term list to recover messages on diseases):
-    start_words_file = open(path_to_language_data + 'start_words.txt', 'r')
-    start_words = []
-    for l in start_words_file:
-        l = l.strip()
-        l = l.lower()
-        start_words.append(l)
-    start_words_file.close()
-    language_data['start_words'] = start_words
+    return grammar
+
+def language_data_loader(grammar_path, start_words_path, stop_words_path):
+    """
+    It receives three file paths as input:
+    - Grammar
+    - Start words
+    - Stop words
+    Then it parses them and returns an array with their contents
+    altogether.
+    """
+    language_data = dict()
+    # Load grammar
+    language_data['grammar'] = file_parser(grammar_path, False)
+    # Load start words (a term list to recover messages on diseases)
+    language_data['start_words'] = file_parser(start_words_path, True)
     # Load stop words (words tagged as noun phrases that cannot be extracted
     # as entities (e.g. You, @username11):
-    stop_words_file = open(path_to_language_data + 'stop_words.txt', 'r')
-    stop_words = []
-    for l in stop_words_file:
-        l = l.strip()
-        l = l.lower()
-        stop_words.append(l)
-    stop_words_file.close()
-    language_data['stop_words'] = stop_words
+    language_data['stop_words'] = file_parser(stop_words_path, True)
     return language_data
-
-# (2) 'start word' finder:
 
 
 def start_word_match(message, start_word_list):
+    """
+    'start word' finder:
+    """
     start_word = ''
     for w in start_word_list:
         if re.search(w, message):
@@ -82,12 +77,15 @@ def start_word_match(message, start_word_list):
     elif len(start_word) == 0:
         return '<No start word in message>'
 
-# (3) Analyzer (treatment-entity finder)
-# The input grammar follows two basic syntactic schemes, where X = treatment/solution and Y = disease/problem:
-# (a) X to treat Y -> X comes first, (b) Y treated with X -> Y comes first.
 
-
-def analyzer(message, start_words, grammar, stop_words):
+def analyzer(message, start_words, stop_words, grammar, nlp):
+    """
+    Analyzer (treatment-entity finder)
+    The input grammar follows two basic syntactic schemes,
+    where X = treatment/solution and Y = disease/problem:
+    (a) X to treat Y -> X comes first,
+    (b) Y treated with X -> Y comes first.
+    """
     # Find the start word in message:
     start_word = start_word_match(message, start_words)
     twitter_start_word = '(' + '#' + start_word + '|' + start_word + ')'
