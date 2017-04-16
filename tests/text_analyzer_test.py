71bd0d8d48aaf394180aaaab5882b119670f4b94
F. Javier R. Donado <fjrd84@gmail.com>
2017-04-16 13:20:15 +0200

    Test file parser.

--- tests/text_analyzer_test.py
+++ tests/text_analyzer_test.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+Text analyzer tests.
+"""
+from engines import text_analyzer
 
-# def test_start_word_match(message,start_word_list):
-#     result = start_word_match(message,start_word_list)
-#     if type(result) == unicode:
-#         if len(result) > 0:
-#             print('OK (test_start_word_match)')
-#     else:
-#         print('ERROR (test_start_word_match)')
 
-# def test_analyzer(message,start_word_list,grammar,stop_word):
-#     result = analyzer(message,start_word_list,grammar,stop_word)
-#     if result == '<nothing found>':
-#         print('OK (test_analyzer)')
-#     elif type(result) == unicode:
-#         if len(result) > 0:
-#             print('OK (test_analyzer)')
-#     else:
-#         print('ERROR (test_analyzer)')
+def test_file_parser():
+    """
+    File parser tests.
+    """
+    start_words = text_analyzer.file_parser('./tests/demo_start_words.txt', True)
+    assert 'negativity' in start_words
+    assert 'pci' in start_words
+    assert r'acute \w+' in start_words
+    assert 'eczema' in start_words
+    assert 'wrinkles' in start_words
+    assert 'yeast infections' in start_words
+    assert 'wrinkless' not in start_words
+    assert 'invented_word' not in start_words
